cc105d034b8673629f8a7f5a96ad1c717825f88c
unknown <alamyrjunior@gmail.com>
2023-12-10 11:25:59 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true
-  }
-   
+  output: "export"
+
 }
 
 module.exports = nextConfig
